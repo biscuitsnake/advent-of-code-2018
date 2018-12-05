53a4890f6e584a644acb48c52c29aaf729a4a87a
biscuitsnake <anudookna@gmail.com>
2018-12-05 17:31:22 +0000

    combined 5a + 5b, fixed silent index error

--- 5.py
+++ 5.py
@@ -1,12 +1,38 @@
 import string
 
+# part 1 = 9900
+
 file = open('5.txt')
 polymer = list(file.read())
 polymer.pop()
 
-types = {}
+reaction = True
+totalDes = 0
 
+while reaction:
+    destroyed = False    
+    i = 0
+    r = True
+    
+    while r:
+        f = polymer[i]
+        s = polymer[i+1]
+        if (f.isupper() != s.isupper()) and (f.lower() == s.lower()):
+            del polymer[i:i+2]
+            destroyed = True
+            totalDes += 2
+        i += 1
+        if (i >= (len(polymer)) - 1):
+            r = False
+
+    if not destroyed:
+        print (len(polymer))
+        reaction = False
+
+# part 2 = 4992
 # hashtag slow
+
+types = {}
 
 for letter in string.ascii_lowercase:
     unit = letter.upper() + letter # Aa
@@ -28,16 +54,28 @@ for letter in string.ascii_lowercase:
                 destroyed = True
                 totalDes += 2
             i += 1
-            if (i >= (len(new_polymer)) - 2):
+            if (i >= (len(new_polymer)) - 1):
                 r = False
 
         if not destroyed:
-            # print (len(new_polymer) - 2)
-            types[unit] = (len(new_polymer) - 2)
+            types[unit] = (len(new_polymer))
             reaction = False
 
-print (min(types.values()))   
+print (min(types.values())) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        
 
     
-    
-            
