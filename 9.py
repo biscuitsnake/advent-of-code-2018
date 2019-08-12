932b57a0ca6c4c194490a4328fcaec91d6bf8e59
biscuitsnake <anudookna@gmail.com>
2019-08-12 19:22:31 +0100

    i grew up and learnt how to use deques

--- 9.py
+++ 9.py
@@ -1,44 +1,44 @@
 import collections
 
+def game(elves, last_marble):
+    players = collections.defaultdict(int)
+    current_player = 1
+
+    current_marble = 0
+    circle = collections.deque([0])
+
+    for i in range(1, last_marble+1):
+        if len(circle) == 1:
+            current_marble = 1
+            circle.insert(1, i)
+
+        elif i % 23 == 0:
+            players[current_player] += i
+            circle.rotate(7)
+            players[current_player] += circle.pop()
+            circle.rotate(-1)
+
+        else:
+            circle.rotate(-1)
+            circle.append(i)
+        
+        i += 1
+        if current_player == elves:
+            current_player = 1
+        else:
+            current_player += 1
+
+    return max(players.values())
+
+
 # part 1 = 416424
 
 p = 413
 last = 71082
+print (game(p, last))
 
-players = collections.defaultdict(int)
-current_player = 1
+# part 2 = 3498287922
 
-current_marble = 0
-circle = [0]
-
-for i in range(1, last+1):
-    if len(circle) == 1:
-        current_marble = 1
-        circle.insert(1, i)
-
-    elif i % 23 == 0:
-        players[current_player] += i
-
-        if (current_marble - 7) < 0:
-            players[current_player] += circle.pop(len(circle) - (7-current_marble))
-            current_marble = len(circle) - (7-current_marble) + 1
-        else:
-            players[current_player] += circle.pop((current_marble) - 7)
-            current_marble = current_marble - 7
-
-    else:
-        current_marble = current_marble + 2
-        if current_marble <= len(circle):
-            circle.insert(current_marble, i)
-        else:
-            current_marble = (current_marble) % len(circle)
-            circle.insert(current_marble, i)
-    
-    i += 1    
-    if current_player == p:
-        current_player = 1
-    else:
-        current_player += 1
-
-print (max(players.values()))
-
+p = 413
+last = 71082 * 100
+print(game(p, last))
