824b111db0a4dbfd85598911a32225c673deb9d9
biscuitsnake <anudookna@gmail.com>
2020-12-03 09:03:11 +0000

    day 2, but less bad

--- 2020/02.py
+++ 2020/02.py
@@ -1,19 +1,13 @@
 puzzle = [i.strip().split(" ") for i in open("02.txt").readlines()]
 
-valid = 0
+valid1, valid2 = 0, 0
 for line in puzzle:
     lower = int(line[0].split("-")[0])
     upper = int(line[0].split("-")[1])
     if lower <= line[2].count(line[1][0]) <= upper:
-        valid += 1
+        valid1 += 1
+    if (line[2][lower-1] == line[1][0]) != (line[2][upper-1] == line[1][0]):
+        valid2 += 1
 
-print(valid)
-
-valid = 0
-for line in puzzle:
-    lower = int(line[0].split("-")[0]) - 1
-    upper = int(line[0].split("-")[1]) - 1
-    if (line[2][lower] == line[1][0]) != (line[2][upper] == line[1][0]):
-        valid += 1
-
-print(valid)
+print(valid1)
+print(valid2)
